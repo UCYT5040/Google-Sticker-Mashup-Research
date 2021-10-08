15c9d9ef151ec16d1138aa302ede78eaea625116
Jeremiah <46662314+UCYT5040@users.noreply.github.com>
2021-10-08 16:10:30 -0500

    Update getallimgs.py

--- getallimgs.py
+++ getallimgs.py
@@ -1,25 +1,31 @@
-all_emojis = open("emojis.txt").read()
-import itertools, requests
+from urllib.parse import quote
+import json
 
-for emoji1, emoji2 in list(itertools.combinations(all_emojis, 2)):
-    e1 = hex(ord(emoji1))[2:]
-    e2 = hex(ord(emoji2))[2:]
-    filename = "all_emojis/u{0}_u{1}.png"
-    url = "https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u{0}/u{0}_u{1}.png"
-    image = url.format(e1, e2)
-    r = requests.get(url.format(e1, e2), allow_redirects=True)
-    if r.headers.get('content-type') == 'image/png':open(filename.format(e1, e2), "wb").write(r.content)
-for emoji1 in all_emojis:
-    e1 = hex(ord(emoji1))[2:]
-    e2 = hex(ord(emoji2))[2:]
-    filename = "all_emojis/u{0}_u{1}.png"
-    url = "https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u{0}/u{0}_u{1}.png"
-    image = url.format(e1, e2)
-    r = requests.get(url.format(e1, e2), allow_redirects=True)
-    if r.headers.get('content-type') == 'image/png':open(filename.format(e1, e2), "wb").write(r.content)
-    filename = "u{0}_u{1}.png"
-    url = "https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u{0}/u{0}_u{1}.png"
-    image = url.format(e1, e2)
-    url = "https://www.facebook.com/favicon.ico"
-    r = requests.get(url, allow_redirects=True)
-    open(filename.format(e1, e2), "wb").write(r.content)
+import requests
+
+api_url_format = "https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q={}_{}"
+
+with open("emojis.txt") as f:
+    emojis = set(f.read().strip())
+
+image_urls = {}
+
+for emoji1 in emojis:
+    for emoji2 in emojis:
+        print(f"{ord(emoji1):x}_{ord(emoji2):x} ({emoji1} + {emoji2})")
+        api_url = api_url_format.format(quote(emoji1), quote(emoji2))
+        data = requests.get(api_url).json()
+        results = data["results"]
+        if not results:
+            print("  - skipped")
+            continue
+        url = results[0]["media_formats"]["png_transparent"]["url"]
+        fp = url.split('/')[-1]
+        if fp in image_urls:
+            print("  + duplicate")
+        image_urls[fp] = url
+
+with open("image_urls.json", "w") as f:
+    json.dump(image_urls, f, indent=2)
+
+
