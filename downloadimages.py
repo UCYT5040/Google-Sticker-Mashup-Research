95b18f88d46745cc776c6f4888170629e13ef1f6
Jeremiah <46662314+UCYT5040@users.noreply.github.com>
2021-10-08 18:54:38 -0500

    Update downloadimages.py

--- downloadimages.py
+++ downloadimages.py
@@ -1,8 +1,19 @@
 import json, grequests
-urls = json.load(open("image_urls.json", "r"))
-urls = dict([(value, key) for key, value in urls.items()])
-responses = (grequests.get(url) for url in urls.keys())
-responses = grequests.map(responses)
-for r in responses:
-  filename = f"stickers/{urls[r.url]}"
-  open(filename, "wb").write(r.content)
+from requests_threads import AsyncSession
+
+session = AsyncSession(n=100)
+
+async def _main():
+    rs = []
+    urls = json.load(open("image_urls.json", "r"))
+    urls = dict([(value, key) for key, value in urls.items()])
+    for url in urls.keys():
+        rs.append(await session.get(url))
+    print(rs)
+    for r in rs:
+      filename = f"stickers/{urls[r.url]}"
+      open(filename, "wb").write(r.content)
+
+if __name__ == '__main__':
+    session.run(_main)
+    
