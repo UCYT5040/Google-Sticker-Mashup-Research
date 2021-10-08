10fb789603a8d86a04ecac88ec450d04fed91f82
Jeremiah <46662314+UCYT5040@users.noreply.github.com>
2021-10-08 20:42:18 +0000

    fixed weird issue

--- getallimgs.py
+++ getallimgs.py
@@ -4,7 +4,6 @@ import itertools, requests
 for emoji1, emoji2 in list(itertools.combinations(all_emojis, 2)):
     e1 = hex(ord(emoji1))[2:]
     e2 = hex(ord(emoji2))[2:]
-<<<<<<< HEAD
     filename = "all_emojis/u{0}_u{1}.png"
     url = "https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u{0}/u{0}_u{1}.png"
     image = url.format(e1, e2)
@@ -18,11 +17,9 @@ for emoji1 in all_emojis:
     image = url.format(e1, e2)
     r = requests.get(url.format(e1, e2), allow_redirects=True)
     if r.headers.get('content-type') == 'image/png':open(filename.format(e1, e2), "wb").write(r.content)
-=======
     filename = "u{0}_u{1}.png"
     url = "https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u{0}/u{0}_u{1}.png"
     image = url.format(e1, e2)
     url = "https://www.facebook.com/favicon.ico"
     r = requests.get(url, allow_redirects=True)
     open(filename.format(e1, e2), "wb").write(r.content)
->>>>>>> origin/main
