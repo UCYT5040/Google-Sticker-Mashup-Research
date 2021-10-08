4072e333450e79aa8d02d717e9d2c206c7f3d9a1
Jeremiah <46662314+UCYT5040@users.noreply.github.com>
2021-10-08 16:57:36 -0500

    Update getallimgs.py

--- getallimgs.py
+++ getallimgs.py
@@ -12,18 +12,32 @@ image_urls = {}
 
 for emoji1 in emojis:
     for emoji2 in emojis:
-        print(f"{ord(emoji1):x}_{ord(emoji2):x} ({emoji1} + {emoji2})")
-        api_url = api_url_format.format(quote(emoji1), quote(emoji2))
-        data = requests.get(api_url).json()
-        results = data["results"]
-        if not results:
-            print("  - skipped")
-            continue
-        url = results[0]["media_formats"]["png_transparent"]["url"]
-        fp = url.split('/')[-1]
-        if fp in image_urls:
-            print("  + duplicate")
-        image_urls[fp] = url
+        try:
+            print(f"{ord(emoji1):x}_{ord(emoji2):x} ({emoji1} + {emoji2})")
+            api_url = api_url_format.format(quote(emoji1), quote(emoji2))
+            data = requests.get(api_url).json()
+            results = data["results"]
+            if not results:
+                print("  - skipped")
+                continue
+            url = results[0]["media_formats"]["png_transparent"]["url"]
+            fp = url.split('/')[-1]
+            if fp in image_urls:
+                print("  + duplicate")
+            image_urls[fp] = url
+         except BaseException:
+            print(f"{ord(emoji1):x}_{ord(emoji2):x} ({emoji1} + {emoji2})")
+            api_url = api_url_format.format(quote(emoji1), quote(emoji2))
+            data = requests.get(api_url).json()
+            results = data["results"]
+            if not results:
+                print("  - skipped")
+                continue
+            url = results[0]["media_formats"]["png_transparent"]["url"]
+            fp = url.split('/')[-1]
+            if fp in image_urls:
+                print("  + duplicate")
+            image_urls[fp] = url
 
 with open("image_urls.json", "w") as f:
     json.dump(image_urls, f, indent=2)
